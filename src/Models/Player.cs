ccd6b66a21d4a5f594a7dd247a2db1f80c9acdbc
hasinajy <jyhasina@gmail.com>
2025-01-14 23:35:01 +0300

    feat: Create Player model

---
+++ src/Models/Player.cs
@@ -0,0 +1,20 @@
+using System.Drawing;
+
+namespace Models
+{
+    public class Player
+    {
+        public Point Position { get; set; }
+        public TeamType Team { get; set; }
+        public bool HasBall { get; set; }
+        public bool IsOffside { get; set; }
+
+        public Player(Point position, TeamType team)
+        {
+            Position = position;
+            Team = team;
+            HasBall = false;
+            IsOffside = false;
+        }
+    }
+}
